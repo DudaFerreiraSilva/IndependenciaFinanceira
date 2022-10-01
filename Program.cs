ed0334211943d5024d7eaecccd71a5b5532e47d6
Eduarda Ferreira <edududaferreira10@gmail.com>
2022-10-01 16:26:35 -0300

    Resolução

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿decimal gastosMensais, valorAInvestir;
+double dividendYield;
+
+Console.WriteLine("--- Vivendo de dividendos ---\n");
+    
+    Console.Write("Gastos mensais (em R$)....: ");
+    gastosMensais = Convert.ToDecimal(Console.ReadLine());
+
+Console.Write("Rendimentos mensais (%)...: ");
+dividendYield = Convert.ToDouble(Console.ReadLine());
+
+valorAInvestir = gastosMensais / Convert.ToDecimal(dividendYield / 100);
+
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine($"\nPara receber {gastosMensais:C} por mês com rendimentos de {dividendYield:N1}% você precisa ter investido {valorAInvestir:C}.");
+Console.ResetColor();
+
+
